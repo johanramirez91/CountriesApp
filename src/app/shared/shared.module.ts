19a0b561970d0f5f7a3246df633557d8257d243b
johan.ramirez <johan.ramirez@sofka.com.co>
2022-04-07 17:04:23 -0500

    [Added] footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [CommonModule],
+  exports: [FooterComponent],
 })
 export class SharedModule {}
