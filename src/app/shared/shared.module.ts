ca28d90f8b8446cc35cc51eb916b0784c526f6dc
johan.ramirez <johan.ramirez@sofka.com.co>
2022-04-07 21:33:50 -0500

    [Refactor]: sidebar component isolated and styles fixed

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [FooterComponent],
-  imports: [CommonModule],
-  exports: [FooterComponent],
+  declarations: [FooterComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [FooterComponent, SidebarComponent],
 })
 export class SharedModule {}
