928df3f0638489de8ac8a639a79563b9e1430759
johan.ramirez <johan.ramirez@sofka.com.co>
2022-04-07 19:27:21 -0500

    [Added]: Routes added in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { PorCapitalComponent } from './country/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from './country/pages/por-pais/por-pais.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PorRegionComponent } from './country/pages/por-region/por-region.component';
+import { VerPaisComponent } from './country/pages/ver-pais/ver-pais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PorPaisComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'region',
+    component: PorRegionComponent,
+  },
+  {
+    path: 'capital',
+    component: PorCapitalComponent,
+  },
+  {
+    path: 'pais/:id',
+    component: VerPaisComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
